f3e2ab526a5039115fb344791942496cf0e90c53
SimonVanMello <simon.vanmello@proton.me>
2023-11-01 19:59:32 +0100

    event -> keystates

--- pong.c
+++ pong.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
+#include <stdbool.h>
 #include <SDL2/SDL.h>
 
 const int WIDTH = 800, HEIGHT = 600;
+const int FPS = 30, FRAME_DELAY = 1000/FPS;
+
+Uint32 frame_start;
+int frame_time;
 
 int main( int argc, char *argv[] )
 {
@@ -18,63 +23,75 @@ int main( int argc, char *argv[] )
     SDL_Renderer *renderer = SDL_CreateRenderer( window, -1, 0 );
 
     SDL_SetRenderDrawColor( renderer, 0, 0, 0, 255 );
-
     SDL_RenderClear( renderer );
-
     SDL_RenderPresent( renderer );
 
-    SDL_Event event;
+    bool playing = true;
+    
 
-    while ( 1 )
+    while ( playing )
     {
-        if ( SDL_PollEvent( &event ) )
+        // Stores the number of ticks at the start of the loop
+        frame_start = SDL_GetTicks();
+
+        SDL_Event event;
+        while ( SDL_PollEvent( &event ) )
         {
-            if ( SDL_QUIT == event.type )
+            if (event.type == SDL_QUIT)
             { break; }
 
-            if ( SDL_KEYDOWN == event.type )
-            {	//down movement
-                if ( SDLK_j == event.key.keysym.sym )
-                { printf( "J is pressed - Move down\n");}
-                if ( SDLK_s == event.key.keysym.sym )
-                { printf("S is pressed - Move down\n"); }
-                if ( SDLK_DOWN == event.key.keysym.sym )
-                { printf("down key is pressed - Move down\n"); }
+            // if ( SDL_KEYDOWN == event.type )
+            // {	
+                // down movement
+            // }
+            // else if ( SDL_KEYUP == event.type )
+            // {
+		    //     // down movement
+            //     if ( SDLK_j == event.key.keysym.sym )
+            //     { printf("J is not pressed - Stop moving down\n"); }
+            //     else if ( SDLK_s == event.key.keysym.sym )
+            //     { printf("S is not pressed - Stop moving down\n"); }
+            //     else if ( SDLK_DOWN == event.key.keysym.sym )
+            //     { printf("down key is not pressed - Stop moving down\n"); }
 
-		//up movement
-		
-                if ( SDLK_k == event.key.keysym.sym )
-                { printf( "K is pressed - Move up\n");}
-                if ( SDLK_w == event.key.keysym.sym )
-                { printf("W is pressed - Move up\n"); }
-                if ( SDLK_UP == event.key.keysym.sym )
-                { printf("up key is pressed - Move up\n"); }
-            }
-            else if ( SDL_KEYUP == event.type )
-            {
-		//down movement
-                if ( SDLK_j == event.key.keysym.sym )
-                { printf("J is not pressed - Stop moving down\n"); }
-                if ( SDLK_s == event.key.keysym.sym )
-                { printf("S is not pressed - Stop moving down\n"); }
-                if ( SDLK_DOWN == event.key.keysym.sym )
-                { printf("down key is not pressed - Stop moving down\n"); }
+		    //     // up movement
+            //     if ( SDLK_k == event.key.keysym.sym )
+            //     { printf("K is not pressed - Stop moving down\n"); }
+            //     else if ( SDLK_w == event.key.keysym.sym )
+            //     { printf("W is not pressed - Stop moving down\n"); }
+            //     else if ( SDLK_UP == event.key.keysym.sym )
+            //     { printf("up key is not pressed - Stop moving down\n"); }
+            // }
+        }
 
-		//up movement
-		//
+        const Uint8* keystates = SDL_GetKeyboardState(NULL);
 
-                if ( SDLK_k == event.key.keysym.sym )
-                { printf("K is not pressed - Stop moving down\n"); }
-                if ( SDLK_w == event.key.keysym.sym )
-                { printf("W is not pressed - Stop moving down\n"); }
-                if ( SDLK_UP == event.key.keysym.sym )
-                { printf("up key is not pressed - Stop moving down\n"); }
-            }
+        if (keystates[SDL_SCANCODE_J])
+        { printf("J is pressed - Move down\n");}
+        else if (keystates[SDL_SCANCODE_S])
+        { printf("S is pressed - Move down\n"); }
+        else if (keystates[SDL_SCANCODE_DOWN])
+        { printf("down key is pressed - Move down\n");}
+
+        // up movement
+        if (keystates[SDL_SCANCODE_K])
+        { printf( "K is pressed - Move up\n");}
+        else if (keystates[SDL_SCANCODE_W])
+        { printf("W is pressed - Move up\n"); }
+        else if (keystates[SDL_SCANCODE_UP])
+        { printf("up key is pressed - Move up\n"); }
+
+        // This measures how long this iteration of the loop took
+        frame_time = SDL_GetTicks() - frame_start;
+        // This keeps us from displaying more frames than 30/Second
+        if (FRAME_DELAY > frame_time)
+        {
+            SDL_Delay(FRAME_DELAY - frame_time);
         }
     }
 
     SDL_DestroyWindow( window );
-    SDL_Quit( );
+    SDL_Quit();
 
     return EXIT_SUCCESS;
 }
