7dc05e7997299df95f871311d3ce0ccb6789f531
SimonVanMello <simon.vanmello@proton.me>
2023-11-02 11:31:47 +0100

    fix not appearing player1

--- pong.c
+++ pong.c
@@ -22,8 +22,10 @@ int render_setup(){
 
     rect_player1.x = 30;
     rect_player2.x = WIDTH-34;
-    rect_player1.w, rect_player2.w = 4;
-    rect_player1.h, rect_player2.h = 50;
+    rect_player1.w= 4;
+    rect_player2.w = 4;
+    rect_player1.h = 50;
+    rect_player2.h = 50;
 
     return 0;
 }
